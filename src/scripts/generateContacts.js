0a22e7a8251908bdffef944443bc419bcbf88305
Iryna Oliinyk <arairishka2@gmail.com>
2024-10-15 18:48:17 +0200

    task 1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,21 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact';
+import { PATH_DB } from '../constants/contacts';
+import fs from 'node:fs/promises';
+
+export const generateContacts = async (number) => {
+  let contact = [];
+  for (let i = 0; i < number; i++) {
+    contact.push(createFakeContact());
+  }
+  try {
+    const dbData = await fs.readFile(PATH_DB, 'utf8');
+    const dbContact = JSON.parse(dbData);
+    dbContact.push(...contact);
+    await fs.writeFile(PATH_DB, JSON.stringify(dbContact));
+    console.log(`${number} contacts generated and added to db.json`);
+  } catch (error) {
+    console.log('Помилка:', error);
+  }
+};
 
 generateContacts(5);
